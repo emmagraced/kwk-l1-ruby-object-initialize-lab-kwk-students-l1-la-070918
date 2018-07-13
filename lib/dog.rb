2f3c81b32f10c4f32b7c3c7046253c0d79de1652
Emma Davis <emmadavis01@gmail.com>
2018-07-13 21:49:34 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,18 @@
-# dog.rb
+class Dog 
+  attr_accessor :name, :breed
+  def initialize(name, breed)
+    @name = name 
+    @breeds = breed
+  def breed
+      if @breeds == ""
+        breed = "mutt"
+      else breed = false
+        breed = "golden retriever"
+      end
+  end
+end
+end
+a_dog=Dog.new("Teddy","")
+puts a_dog.name
+puts a_dog.breed
+
--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,8 @@
-# person.rb
+class Person
+  attr_accessor :name 
+  def initialize(name)
+    @name = name
+  end
+end
+  a_person=Person.new("Person")
+puts a_person.name
